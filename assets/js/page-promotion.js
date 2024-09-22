475857abd4f4512cfeee5cb1f8a2f23c8b7f442e
SabineLouisJean <e2495208@cmaisonneuve.qc.ca>
2024-09-22 08:58:56 -0400

    Premiere version

---
+++ assets/js/page-promotion.js
@@ -0,0 +1,31 @@
+// Variables globale
+let promoTitle = ["6 à 6", "Le ptit 2", "Bonne fête!"];
+let promoText = ["Obtenez 6 beignets pour 6 dollars,tous les samedis.", " Tous les cupcakes sont a 2$ jusqu'a 14h00, tous les mardis", "Obtenez 50% de rabais sur un gateau le jour de votre anniversaire."];
+let promoImageCarousel = [];
+
+//Element HTML
+
+
+
+function init (){
+
+}
+
+function afficherTitreEtTexte (){}
+
+function avancerBoutton() {
+
+}
+
+function reculerBoutton (){
+
+}
+
+
+
+
+
+
+
+
+//init();
---
+++ assets/js/page.produits.js
@@ -0,0 +1,3 @@
+const produitVedetteImage = document.querySelector(".produit-vedette-image");
+const produitVedetteTitre = document.querySelector(".produit-vedette-titre");
+const produitVedetteTexte = document.querySelector(".produit-vedette-texte");
