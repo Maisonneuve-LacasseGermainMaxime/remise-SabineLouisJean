d61893a7b9d37537e2382f51a5f430b0010bde79
SabineLouisJean <e2495208@cmaisonneuve.qc.ca>
2024-09-24 15:25:49 -0400

    2ieme Version

--- assets/js/page-promotion.js
+++ assets/js/page-promotion.js
@@ -3,6 +3,30 @@ let promoTitle = ["6 à 6", "Le ptit 2", "Bonne fête!"];
 let promoText = ["Obtenez 6 beignets pour 6 dollars,tous les samedis.", " Tous les cupcakes sont a 2$ jusqu'a 14h00, tous les mardis", "Obtenez 50% de rabais sur un gateau le jour de votre anniversaire."];
 let promoImageCarousel = [];
 
+
+const listePromotion = [
+    {
+       id: 1,
+       titre: "6 à 6",
+       texte: "Obtenez 6 beignets pour 6 dollars,tous les samedis.",
+       lienImage: "assets/img/promo-donuts.jpg",
+    },
+
+    {
+        id: 2,
+        titre: "Le ptit 2",
+       texte: "Tous les cupcakes sont a 2$ jusqu'a 14h00, tous les mardis",
+       lienImage: "assets/img/promo-cupcake.jpg",
+    },
+
+    {
+        id: 3,
+        titre: "Bonne fête!",
+       texte: "Obtenez 50% de rabais sur un gateau le jour de votre anniversaire.",
+       lienImage: "assets/img/promo-birthday.jfif",
+    }
+];
+
 //Element HTML
 
 
--- assets/js/page.produits.js
+++
@@ -1,3 +0,0 @@
-const produitVedetteImage = document.querySelector(".produit-vedette-image");
-const produitVedetteTitre = document.querySelector(".produit-vedette-titre");
-const produitVedetteTexte = document.querySelector(".produit-vedette-texte");
